254293442aea517b436b7de1a94126cca08d783d
Matt Wlazlo <mwlazlo@gmail.com>
2017-01-21 09:00:26 +1100

    STG-1 fixed up fields

--- slope-test-generator/src/main/java/com/tvm/stg/ConfigForm.java
+++ slope-test-generator/src/main/java/com/tvm/stg/ConfigForm.java
@@ -1,7 +1,19 @@
 package com.tvm.stg;
 
+import javax.swing.*;
+
 /**
  * Created by horse on 21/1/17.
  */
 public class ConfigForm {
+    private JButton loadSymbolsButton;
+    private JList symbolsList;
+    private JTextField dataDirText;
+    private JTable compConfigTable;
+    private JTable pointsConfigTable;
+    private JSpinner pointsSpinner;
+    private JTable slopeConfigTable;
+    private JButton saveBtn;
+    private JComboBox configCombo;
+    private JButton newBtn;
 }
